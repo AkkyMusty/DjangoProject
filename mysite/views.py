fc81c3c63baf94e86a02897081e96a5cf2ab4fb4
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-10-13 23:31:32 +0200

    Initial project set up and url routing

---
+++ mysite/views.py
@@ -0,0 +1,18 @@
+"""
+To render html web pages
+"""
+from django.http import HttpResponse
+
+
+HTML_STRING = """
+<h1>Hello World</h1>
+"""
+
+
+def home_view(request):
+    """
+    Take in a request (Django sends a request)
+    Return HTML as a response
+    """
+
+    return HttpResponse(HTML_STRING)
