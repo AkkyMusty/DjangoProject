149459e3d2c4b8c19fc760fd8848421982f7f9f8
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-10-23 23:45:42 +0200

    Model form implemented for article model

--- articles/views.py
+++ articles/views.py
@@ -25,23 +25,44 @@ def article_search_view(request):
     }
     return render(request,"articles/search.html", context=context)
 
+# @login_required
+# def article_create_view(request):
+#     # print(request.POST)
+#     form = ArticleForm()
+#     print(dir(form))
+#     context = {
+#         "form": form
+#     }
+#     if request.method == "POST":
+#         form = ArticleForm(request.POST)
+#         if form.is_valid():
+#             title = form.cleaned_data.get('title')
+#             content = form.cleaned_data.get('content')
+#         # print(title, content)
+#         article_object = Article.objects.create(title=title, content=content)
+#         context["object"] = article_object
+#         context["created"] = True
+#
+#     return render(request, "articles/create.html", context=context)
+
 @login_required
 def article_create_view(request):
     # print(request.POST)
-    form = ArticleForm()
+    form = ArticleForm(request.POST or None)
     print(dir(form))
     context = {
         "form": form
     }
-    if request.method == "POST":
-        form = ArticleForm(request.POST)
-        if form.is_valid():
-            title = form.cleaned_data.get('title')
-            content = form.cleaned_data.get('content')
-        # print(title, content)
-        article_object = Article.objects.create(title=title, content=content)
-        context["object"] = article_object
-        context["created"] = True
+    if form.is_valid():
+        article_object = form.save()
+        context['form'] = ArticleForm()
+
+        # title = form.cleaned_data.get('title')
+        # content = form.cleaned_data.get('content')
+        # # print(title, content)
+        # article_object = Article.objects.create(title=title, content=content)
+        # context["object"] = article_object
+        # context["created"] = True
 
     return render(request, "articles/create.html", context=context)
 
