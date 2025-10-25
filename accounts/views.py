d3da379129c38fb3ee8d5378bfda8a55f33bf9a3
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-10-25 13:09:16 +0200

    UserCreationForm added to register new user

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,15 @@
 from idlelib.rpc import request_queue
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import render, redirect
+
+def register_view(request):
+    form = UserCreationForm(request.POST or None)
+    if form.is_valid():
+        user_obj = form.save()
+        return redirect('/login')
+    context = {"form": form}
+    return render(request, "accounts/register.html", context)
 
 # Create your views here.
 def login_view(request):
@@ -23,5 +32,5 @@ def logout_view(request):
         return redirect("/login/")
     return render(request, "accounts/logout.html", {})
 
-def register_view(request):
-    return render(request, "accounts/register.html", {})
+# def register_view(request):
+#     return render(request, "accounts/register.html", {})
--- mysite/urls.py
+++ mysite/urls.py
@@ -20,6 +20,7 @@ from django.urls import path
 from accounts.views import (
     login_view,
     logout_view,
+    register_view
 )
 from articles.views import (
     article_create_view,
@@ -39,4 +40,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', login_view),
     path('logout/', logout_view),
+    path('register/', register_view),
 ]
