1f02464cffb9c0935f6d36cd293bec06f808f4e8
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-11-03 23:13:13 +0100

    improved on the slugify function using recursive func to autogenerate slug

--- articles/models.py
+++ articles/models.py
@@ -1,12 +1,15 @@
+
 from django.db import models
 from django.db.models.signals import pre_save, post_save
 from django.utils import timezone
 from django.utils.text import slugify
 
+from .utils import slugify_instance_title
+
 # Create your models here.
 class Article(models.Model):
     title = models.CharField(max_length=120)
-    slug = models.SlugField(blank=True, null=True)
+    slug = models.SlugField(unique=True, blank=True, null=True)
     content = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
@@ -16,17 +19,10 @@ class Article(models.Model):
     def save(self, *args, **kwargs):
         # if self.slug is None:
         #     self.slug = slugify(self.title)
+        # if self.slug is None:
+        #     slugify_instance_title(self, save=False)
         super().save(*args, **kwargs)
 
-def slugify_instance_title(instance, save=False):
-    slug = slugify(instance.title)
-    qs = Article.objects.filter(slug=slug).exclude(id=instance.id)
-    if qs.exists():
-        slug = f"{slug}-{qs.count() + 1}"
-    instance.slug = slug
-    if save:
-        instance.save()
-    return instance
 
 def article_pre_save(sender, instance, *args, **kwargs):
     if instance.slug is None:
