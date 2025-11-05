961dfd64b5b900f8ce79b558ffec660881c2bcf1
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-11-05 23:32:53 +0100

    Started writing test for article model

--- articles/tests.py
+++ articles/tests.py
@@ -1,3 +1,18 @@
+from django.template.defaultfilters import title
 from django.test import TestCase
 
 # Create your tests here.
+from .models import Article
+
+class ArticleTestCase(TestCase):
+    def setUp(self):
+        Article.objects.create(title="Hello World", content='Something else')
+
+    def test_queryset_exists(self):
+        qs = Article.objects.all()
+        self.assertTrue(qs.exists())
+
+    def test_queryset_count(self):
+        qs = Article.objects.all()
+        self.assertEqual(qs.count(), 1)
+
