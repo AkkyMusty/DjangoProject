b069a646ca0afd5880a42fd7f1bb78e1ac2e02fd
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-11-07 09:09:34 +0100

    Added new tests to check unique slugs

--- articles/tests.py
+++ articles/tests.py
@@ -1,12 +1,15 @@
 from django.template.defaultfilters import title
 from django.test import TestCase
+from django.utils.text import slugify
 
 # Create your tests here.
 from .models import Article
 
 class ArticleTestCase(TestCase):
     def setUp(self):
-        Article.objects.create(title="Hello World", content='Something else')
+        self.number_of_articles = 5
+        for i in range(0, self.number_of_articles):
+            Article.objects.create(title="Hello World", content='Something else')
 
     def test_queryset_exists(self):
         qs = Article.objects.all()
@@ -14,5 +17,22 @@ class ArticleTestCase(TestCase):
 
     def test_queryset_count(self):
         qs = Article.objects.all()
-        self.assertEqual(qs.count(), 1)
+        self.assertEqual(qs.count(), self.number_of_articles)
+
+    def test_hello_world_slug(self):
+        obj = Article.objects.all().order_by("id").first()
+        title = obj.title
+        slug = obj.slug
+        slugified_title = slugify(title)
+        self.assertEqual(slug, slugified_title)
+
+    def test_hello_world_unique_slug(self):
+        qs = Article.objects.exclude(slug__iexact='hello-world')
+        for obj in qs:
+            title = obj.title
+            slug = obj.slug
+            slugified_title = slugify(title)
+            self.assertNotEqual(slug, slugified_title)
+
+
 
