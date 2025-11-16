c1b87860eb3f0f5ec08279e18136196b682d6333
AkkyMusty <153207112+AkkyMusty@users.noreply.github.com>
2025-11-16 23:17:25 +0100

    Registered the Recipe and Recipe Ingredient models into the admin using admin inlines for foreign keys

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from django.contrib.auth import get_user, get_user_model
 
 # Register your models here.
+from .models import Recipe, RecipeIngredient
+
+# admin.site.register(RecipeIngredient)
+
+User = get_user_model()
+
+class RecipeIngredientInline(admin.StackedInline):
+    model = RecipeIngredient
+    extra = 0
+    # fields =['name', 'quantity', 'unit', 'direction']
+
+class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientInline]
+    list_display = ['name', 'user']
+    readonly_fields = ['timestamp', 'updated']
+    raw_id_fields = ['user']
+
+admin.site.register(Recipe,RecipeAdmin)
+
+
+
+
